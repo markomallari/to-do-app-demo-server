a5592641856289525ce2b16783a4de07a9a20087
Mark Jomel Mallari <MarkJomelO.Mallari@pccw.com>
2024-02-15 07:04:29 +0800

    initial updates

---
+++ server.js
@@ -0,0 +1,173 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const port = 3002;
+
+// Cors configuration - Allows requests from localhost:4200
+const corsOptions = {
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 204,
+  methods: "GET, POST, PUT, DELETE",
+};
+
+// Use cors middleware
+app.use(cors(corsOptions));
+
+// Use express.json() middleware to parse JSON bodies of requests
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/check-account", (req, res) => {
+  const { username, password } = req.body;
+  fs.readFile("database.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    console.log(jsonData);
+    const user = jsonData?.users.filter(
+      (user) => username === user.username && password === user.password
+    );
+
+    res.status(200).json({
+      status: user.length === 1 ? "User exists" : "User does not exist",
+      userExists: user.length === 1,
+    });
+  });
+});
+
+app.get("/tasks", (req, res) => {
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    res.status(200).json({
+      data: jsonData,
+    });
+  });
+});
+
+app.post("/task", (req, res) => {
+  const { name, description, comments } = req.body;
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const maxId = jsonData.data.reduce(
+      (max, item) => Math.max(max, item.id),
+      0
+    );
+
+    const newItem = {
+      id: maxId + 1,
+      name,
+      description,
+      comments,
+    };
+
+    jsonData.data.push(newItem);
+
+    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.status(201).json(newItem);
+    });
+  });
+});
+
+app.put("/task/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, description, comments } = req.body;
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const index = jsonData.data.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      res.status(404).send("Not Found");
+      return;
+    }
+
+    jsonData.data[index] = {
+      id,
+      name,
+      description,
+      comments,
+    };
+
+    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.status(200).json(jsonData.data[index]);
+    });
+  });
+});
+
+app.delete("/task/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const index = jsonData.data.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      res.status(404).send("Not Found");
+      return;
+    }
+
+    jsonData.data.splice(index, 1);
+
+    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.status(204).send();
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
